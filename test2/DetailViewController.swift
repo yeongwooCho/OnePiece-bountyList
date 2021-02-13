135d7d977823ae9ffe9c1e6eebb428327744ac87
yeongwoo <jyy0223@naver.com>
2021-02-13 19:19:35 +0900

    animation update

--- test2/DetailViewController.swift
+++ test2/DetailViewController.swift
@@ -11,12 +11,34 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var imgLabel: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var bountyLabel: UILabel!
+    @IBOutlet weak var nameLabelCenterX: NSLayoutConstraint!
+    @IBOutlet weak var bountyLabelCenterX: NSLayoutConstraint!
     
     let viewModel = DetailViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        prepareAnimation()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+    }
+    
+    func prepareAnimation() {
+        nameLabelCenterX.constant = view.bounds.width
+        bountyLabelCenterX.constant = view.bounds.width
+    }
+    
+    func showAnimation() {
+        nameLabelCenterX.constant = 0
+        bountyLabelCenterX.constant = 0
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: self.view.layoutIfNeeded, completion: nil)
+        
+        UIView.transition(with: imgLabel, duration: 0.5, options: .transitionFlipFromRight, animations: nil, completion: nil)
     }
     
     @IBAction func close(_ sender: Any) {
