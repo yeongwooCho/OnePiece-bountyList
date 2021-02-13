d19de169bbe17235776b675638590506b454ecb5
yeongwoo <jyy0223@naver.com>
2021-02-13 19:32:00 +0900

    animating view properties update

--- test2/DetailViewController.swift
+++ test2/DetailViewController.swift
@@ -28,17 +28,33 @@ class DetailViewController: UIViewController {
     }
     
     func prepareAnimation() {
-        nameLabelCenterX.constant = view.bounds.width
-        bountyLabelCenterX.constant = view.bounds.width
+//        nameLabelCenterX.constant = view.bounds.width
+//        bountyLabelCenterX.constant = view.bounds.width
+        nameLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: view.bounds.width).scaledBy(x: 4, y: 4).rotated(by: 180)
+        nameLabel.alpha = 0
+        
+        bountyLabel.transform = CGAffineTransform(translationX: view.bounds.width, y: -view.bounds.width).scaledBy(x: 4, y: 4).rotated(by: 180)
+        bountyLabel.alpha = 0
+        
     }
     
     func showAnimation() {
-        nameLabelCenterX.constant = 0
-        bountyLabelCenterX.constant = 0
-        
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: self.view.layoutIfNeeded, completion: nil)
+//        nameLabelCenterX.constant = 0
+//        bountyLabelCenterX.constant = 0
+//
+//        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowUserInteraction, animations: self.view.layoutIfNeeded, completion: nil)
+        UIView.animate(withDuration: 10, delay: 0.1, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowAnimatedContent, animations: {
+            self.nameLabel.transform = CGAffineTransform.identity
+            self.nameLabel.alpha = 1
+        }, completion: nil)
+
+        UIView.animate(withDuration: 1, delay: 0.4, usingSpringWithDamping: 0.6, initialSpringVelocity: 2, options: .allowAnimatedContent, animations: {
+            self.bountyLabel.transform = CGAffineTransform.identity
+            self.bountyLabel.alpha = 1
+        }, completion: nil)
         
         UIView.transition(with: imgLabel, duration: 0.5, options: .transitionFlipFromRight, animations: nil, completion: nil)
+
     }
     
     @IBAction func close(_ sender: Any) {
